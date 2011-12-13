2cc0c07c0a76315f4f3ac1f762e1a12949829a0a
Riza Fahmi <rizafahmi@gmail.com>
2011-12-13 14:36:17 +0700

    some updates on arch

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'geofood',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'USER': 'riza',                      # Not used with sqlite3.
         'PASSWORD': '220281',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
